8f7a99c445e3394edb04ca3f9d8fdf252618e5e9
Marcus Hsieh <80995617+MarcusHsieh@users.noreply.github.com>
2024-12-29 01:01:43 -0800

    First complete iteration

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,9 +7,10 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     ui->stackedWidget->setCurrentIndex(0);
-    QFont font = this->font();
-    font.setPointSizeF(font.pointSizeF() * devicePixelRatioF());
-    this->setFont(font);
+    // QFont font = this->font();
+    // font.setPointSizeF(font.pointSizeF() * devicePixelRatioF());
+    // this->setFont(font);
+    updateImage();
 }
 
 MainWindow::~MainWindow()
@@ -17,26 +18,72 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_pushButton_clicked()
+
+void MainWindow::on_tracheostomyButton_clicked()
 {
     ui->stackedWidget->setCurrentIndex(1);
 }
 
 
-void MainWindow::on_pushButton_2_clicked()
+void MainWindow::on_difficultAirwayButton_clicked()
 {
     ui->stackedWidget->setCurrentIndex(2);
 }
 
 
-void MainWindow::on_pushButton_3_clicked()
+void MainWindow::on_nTracheostomyButton_clicked()
 {
     ui->stackedWidget->setCurrentIndex(3);
 }
 
 
-void MainWindow::on_pushButton_4_clicked()
+void MainWindow::on_LTRButton_clicked()
 {
     ui->stackedWidget->setCurrentIndex(4);
 }
 
+// void MainWindow::updateImage() {
+//     // load pixmap
+//     // QPixmap pixmap(":/images/nemours_logo");
+//     QPixmap pixmap("C:/_Projects/safe-airway/nemours_logo.png");
+//     if (pixmap.isNull()) {
+//         qDebug() << "fail";
+//         return;
+//     }
+//     qDebug() << "loaded, size = " << pixmap.size();
+
+//     // set button icon
+//     QIcon icon(pixmap);
+//     if (icon.isNull()) {
+//         qDebug() << "fail ";
+//         return;
+//     }
+
+//     ui->logoButton->setIcon(icon);
+//     // ui->logoButton->setIconSize(QSize(50, 50));
+//     ui->logoButton->setFlat(true);
+//     qDebug() << "good";
+// }
+
+void MainWindow::updateImage() {
+    // QPixmap pixmap("C:/_Projects/safe-airway/images/nemours_logo.png");
+    QPixmap pixmap(":/images/nemours_logo.png");
+    if (!pixmap.isNull()) {
+        ui->logoLabel->setPixmap(pixmap.scaled(
+            ui->logoLabel->size(),
+            Qt::KeepAspectRatio,
+            Qt::SmoothTransformation
+            ));
+        ui->logoLabel->setScaledContents(true);
+    } else {
+        qDebug() << "fail";
+    }
+}
+
+
+
+void MainWindow::resizeEvent(QResizeEvent *event)
+{
+    QMainWindow::resizeEvent(event);
+    updateImage();
+}

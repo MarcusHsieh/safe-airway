a05ca7d1da110b3a7c7b3d67ba74ee81b529c5c1
WobbuffetForUbers <school@hsieh.org>
2024-12-29 00:28:56 -0500

    Buttons Added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,26 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QScreen>
+#include <QGuiApplication>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    // Get the screen geometry
+    QScreen *screen = QGuiApplication::primaryScreen();
+    QRect screenGeometry = screen->geometry();
+    int screenWidth = screenGeometry.width();
+    int screenHeight = screenGeometry.height();
+    this->showFullScreen();
+    // Resize the main window to fit the screen
+    this->resize(screenWidth, screenHeight);
+    // Set size policies for widgets to expand
+    ui->pushButton->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    ui->pushButton_2->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    ui->pushButton_3->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    ui->pushButton_4->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
     ui->stackedWidget->setCurrentIndex(0);
 }
 
